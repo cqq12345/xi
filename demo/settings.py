748f2121d10c0b9f598bf64aecb8e8c32b191642
cqq12345 <1274638001@qq.com>
2019-11-26 10:33:01 +0800

    添加了一个模块

--- demo/settings.py
+++ demo/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'follow',
 ]
 
 ROOT_URLCONF = 'demo.urls'
