1729b567051d8dd2bb922cf02206559cc1556014
cqq12345 <1274638001@qq.com>
2019-11-26 10:31:09 +0800

    添加了一个模块

--- like/models.py
+++ like/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class likw(nodel,Model):
+    id=model.IntegerField()
+    time=model.DateTimeField(auto_now_add=Ture)
+
---
+++ likw/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LikwConfig(AppConfig):
+    name = 'likw'
